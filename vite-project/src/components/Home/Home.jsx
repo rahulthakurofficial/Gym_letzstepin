cf14f68ddaee7634520249c4c368fbfec9d61551
Rahul Kumar Thakur <rahulthakur.official6@gmail.com>
2025-04-29 20:33:29 +0530

    changes

--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -31,7 +31,10 @@ import bookplans from "../../assets/bookplans.png";
 import amenities from "../../assets/amenities.png";
 import safetyHygiene from "../../assets/safetyHygiene.png";
 import cctv from "../../assets/cctv.png";
+import timeIcon from "../../assets/time.png";
 import { FaChevronDown, FaChevronUp, FaStar } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const { addToCart } = useCart();
@@ -49,12 +52,6 @@ const Home = () => {
     updatedUsers[index][field] = value;
     setUsers(updatedUsers);
   };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log("Submitted Users:", users);
-  //   // Send data to backend here
-  // };
 
   const gyms = [
     { name: "Gym 1", address: "Pune", distance: "1.2 km" },
@@ -84,9 +81,6 @@ const Home = () => {
     setActiveIndex(index);
   };
 
- 
-
-
   const faqs = [
     {
       question: "What are your gym operating hours?",
@@ -113,7 +107,8 @@ const Home = () => {
     },
     {
       question: "Do you provide diet or nutrition guidance?",
-      answer: "Yes, our fitness experts offer personalized diet and nutrition plans to support your fitness goals.",
+      answer:
+        "Yes, our fitness experts offer personalized diet and nutrition plans to support your fitness goals.",
     },
   ];
 
@@ -150,6 +145,19 @@ const Home = () => {
     <>
       <div className="container">
         <div className="left-section">
+          <iframe
+            className="map-frame"
+            src="https://www.google.com/maps/embed?pb=!4v1726715106455!6m8!1m7!1sCAoSK0FGMVFpcFBlRlFfcG9fcnJvVDZfZjJXM2FEY0pYa1JyMDBMY21jTzFNTzA.!2m2!1d11.6217778!2d78.11513889999999!3f75.62686526110555!4f-5.441201564066674!5f0.4000000000000002"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            title="Gym Location"
+          ></iframe>
+        </div>
+
+        <div className="right-section">
           <h1 className="logo-title">
             <img src={logo} alt="Gym Logo" className="logo-icon" />
             MK Fitness Club – Pune
@@ -168,20 +176,6 @@ const Home = () => {
             and supportive environment make it easy to stay motivated and
             consistent.
           </p>
-          <button>Call now</button>
-        </div>
-
-        <div className="right-section">
-          <iframe
-            className="map-frame"
-            src="https://www.google.com/maps/embed?pb=!4v1726715106455!6m8!1m7!1sCAoSK0FGMVFpcFBlRlFfcG9fcnJvVDZfZjJXM2FEY0pYa1JyMDBMY21jTzFNTzA.!2m2!1d11.6217778!2d78.11513889999999!3f75.62686526110555!4f-5.441201564066674!5f0.4000000000000002"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            title="Gym Location"
-          ></iframe>
         </div>
       </div>
 
@@ -220,7 +214,6 @@ const Home = () => {
             </ul>
           </div>
         </div>
-        <div className="second-right-section"></div>
       </div>
 
       <div className="fourth-container">
@@ -242,22 +235,19 @@ const Home = () => {
                 <div className="plan-row">
                   <h3>1 Day Plan</h3>
                   <p className="price">₹2,900/month</p>
-                  <img
-                    src={buy}
-                    alt="Buy Now"
-                    className="buy-now"
-                    onClick={() => {
-                      setShowForm(true);
-                    }}
-                  />
-                  <img
-                    src={cart}
-                    alt="Add to Cart"
-                    className="add-to-cart"
+                  <div className="icon-label" onClick={() => setShowForm(true)}>
+                    <FontAwesomeIcon icon={faShoppingBag} className="icon" />
+                    <span className="label">Buy Now</span>
+                  </div>
+                  <div
+                    className="icon-label"
                     onClick={() =>
-                      addToCart({ name: "1 Month Plan", price: 2900 })
+                      addToCart({ name: "1 Day Plan", price: 2900 })
                     }
-                  />
+                  >
+                    <FontAwesomeIcon icon={faCartPlus} className="icon" />
+                    <span className="label">Add to Cart</span>
+                  </div>
                 </div>
               </div>
 
@@ -265,20 +255,19 @@ const Home = () => {
                 <div className="plan-row">
                   <h3>1 Month Plan</h3>
                   <p className="price">₹4,900/month</p>
-                  <img
-                    src={buy}
-                    alt="Buy Now"
-                    className="buy-now"
-                    onClick={() => setShowForm(true)}
-                  />
-                  <img
-                    src={cart}
-                    alt="Add to Cart"
-                    className="add-to-cart"
+                  <div className="icon-label" onClick={() => setShowForm(true)}>
+                    <FontAwesomeIcon icon={faShoppingBag} className="icon" />
+                    <span className="label">Buy Now</span>
+                  </div>
+                  <div
+                    className="icon-label"
                     onClick={() =>
-                      addToCart({ name: "1 Month Plan", price: 2900 })
+                      addToCart({ name: "1 Month Plan", price: 4900 })
                     }
-                  />
+                  >
+                    <FontAwesomeIcon icon={faCartPlus} className="icon" />
+                    <span className="label">Add to Cart</span>
+                  </div>
                 </div>
               </div>
 
@@ -286,20 +275,19 @@ const Home = () => {
                 <div className="plan-row">
                   <h3>6 Months Plan</h3>
                   <p className="price">₹6,900/month</p>
-                  <img
-                    src={buy}
-                    alt="Buy Now"
-                    className="buy-now"
-                    onClick={() => setShowForm(true)}
-                  />
-                  <img
-                    src={cart}
-                    alt="Add to Cart"
-                    className="add-to-cart"
+                  <div className="icon-label" onClick={() => setShowForm(true)}>
+                    <FontAwesomeIcon icon={faShoppingBag} className="icon" />
+                    <span className="label">Buy Now</span>
+                  </div>
+                  <div
+                    className="icon-label"
                     onClick={() =>
-                      addToCart({ name: "1 Year Plan", price: 6900 })
+                      addToCart({ name: "6 Months Plan", price: 6900 })
                     }
-                  />
+                  >
+                    <FontAwesomeIcon icon={faCartPlus} className="icon" />
+                    <span className="label">Add to Cart</span>
+                  </div>
                 </div>
               </div>
 
@@ -307,23 +295,23 @@ const Home = () => {
                 <div className="plan-row">
                   <h3>1 Year Plan</h3>
                   <p className="price">₹6,900/month</p>
-                  <img
-                    src={buy}
-                    alt="Buy Now"
-                    className="buy-now"
-                    onClick={() => setShowForm(true)}
-                  />
-                  <img
-                    src={cart}
-                    alt="Add to Cart"
-                    className="add-to-cart"
+                  <div className="icon-label" onClick={() => setShowForm(true)}>
+                    <FontAwesomeIcon icon={faShoppingBag} className="icon" />
+                    <span className="label">Buy Now</span>
+                  </div>
+                  <div
+                    className="icon-label"
                     onClick={() =>
                       addToCart({ name: "1 Year Plan", price: 6900 })
                     }
-                  />
+                  >
+                    <FontAwesomeIcon icon={faCartPlus} className="icon" />
+                    <span className="label">Add to Cart</span>
+                  </div>
                 </div>
               </div>
             </div>
+
             {showForm && (
               <div className="form-popup">
                 <h2>Buy Plan - User Information</h2>
@@ -381,8 +369,59 @@ const Home = () => {
         </div>
 
         <div className="fourth-right-section">
-          <div className="video-section">
-            <h2>Gym Time Table</h2>
+          <div className="timeTable-section">
+            <div className="timetable-heading">
+              <img src={timeIcon} alt="Time Icon" className="heading-icon" />
+              <h1>Gym Time Table</h1>
+            </div>
+
+            <table className="gym-timetable">
+              <thead>
+                <tr>
+                  <th>Day</th>
+                  <th>Morning (6 AM - 9 AM)</th>
+
+                  <th>Evening (5 PM - 9 PM)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Day">Monday</td>
+                  <td data-label="Morning (6 - 9 AM)">Cardio</td>
+                  <td data-label="Evening (5 - 9 PM)">Zumba</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Tuesday</td>
+                  <td data-label="Morning (6 - 9 AM)">Yoga</td>
+                  <td data-label="Evening (5 - 9 PM)">HIIT</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Wednesday</td>
+                  <td data-label="Morning (6 - 9 AM)">Strength Training</td>
+                  <td data-label="Evening (5 - 9 PM)">Boxing</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Thursday</td>
+                  <td data-label="Morning (6 - 9 AM)">Functional Training</td>
+                  <td data-label="Evening (5 - 9 PM)">Dance Fitness</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Friday</td>
+                  <td data-label="Morning (6 - 9 AM)">Yoga</td>
+                  <td data-label="Evening (5 - 9 PM)">Strength Training</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Saturday</td>
+                  <td data-label="Morning (6 - 9 AM)">Bootcamp</td>
+                  <td data-label="Evening (5 - 9 PM)">Spinning</td>
+                </tr>
+                <tr>
+                  <td data-label="Day">Sunday</td>
+                  <td data-label="Morning (6 - 9 AM)">Stretching</td>
+                  <td data-label="Evening (5 - 9 PM)">Open Gym</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
@@ -455,14 +494,14 @@ const Home = () => {
                 />
                 <span className="facility-label">Swimming Pool</span>
               </li>
-              <li>
+              {/* <li>
                 <img
                   src={Parking}
                   alt="Swimming Pool"
                   className="facility-icon"
                 />
                 <span className="facility-label">Swimming Pool</span>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
@@ -538,14 +577,14 @@ const Home = () => {
                 />
                 <span className="safety-label">CCTV Surveillance</span>
               </li>
-              <li>
+              {/* <li>
                 <img
                   src={Emergency}
                   alt="Emergency Exit"
                   className="safety-icon"
                 />
                 <span className="safety-label">Emergency Exit</span>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
@@ -585,7 +624,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      
 
       <div className="faq-container">
         <h2>Frequently Asked Questions</h2>
